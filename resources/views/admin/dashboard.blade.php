0705e8aabd92400906c903c70bc622bdf10e2854
Janry S <janryseran069407@gmail.com>
2024-07-23 21:30:05 +0800

    added schanesg

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.app2')
 
+@section('title', 'Admin Dashboard')
+
 @section('content')
     <h1>Welcome to the Admin Dashboard</h1>
-    <p>Select an option from the sidebar to get started.</p>
+    <p>This is your admin dashboard content.</p>
 @endsection
--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -3,26 +3,81 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <script src="{{ mix('js/app.js') }}" defer></script>
+    <title>@yield('title')</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="d-flex" id="wrapper">
-        @include('layouts.sidebar2')
+        <!-- Sidebar -->
+        <div class="bg-light border-right" id="sidebar-wrapper">
+            <div class="sidebar-heading">Admin Dashboard</div>
+            <div class="list-group list-group-flush">
+                <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+                <a href="#alumniSubmenu" data-toggle="collapse" aria-expanded="false" class="list-group-item list-group-item-action bg-light">Alumni</a>
+                <ul class="collapse list-unstyled" id="alumniSubmenu">
+                    <li>
+                        <a href="#" class="list-group-item list-group-item-action bg-light">Add</a>
+                    </li>
+                    <li>
+                        <a href="#" class="list-group-item list-group-item-action bg-light">Update</a>
+                    </li>
+                    <li>
+                        <a href="#" class="list-group-item list-group-item-action bg-light">Delete</a>
+                    </li>
+                </ul>
+                <a href="#" class="list-group-item list-group-item-action bg-light">Reports</a>
+            </div>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
         <div id="page-content-wrapper">
             <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                 <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="{{ url('/') }}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('/profile') }}">Profile</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Settings
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ url('/settings') }}">Settings</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ url('/logout') }}">Logout</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </nav>
+
             <div class="container-fluid">
                 @yield('content')
             </div>
         </div>
+        <!-- /#page-content-wrapper -->
     </div>
+    <!-- /#wrapper -->
 
+    <!-- Bootstrap and jQuery JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
     <script>
-        document.getElementById("menu-toggle").addEventListener("click", function() {
-            document.getElementById("wrapper").classList.toggle("toggled");
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
         });
     </script>
 </body>
